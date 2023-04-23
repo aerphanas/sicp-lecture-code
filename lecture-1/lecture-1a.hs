ca3570dd6500036d6c44431bb8c95492e3d2013c
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-04-23 10:14:15 +0700

    add haskell kode for sqrt
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

---
+++ lecture-1/lecture-1a.hs
@@ -0,0 +1,35 @@
+module Main where
+
+import GHC.Float (int2Float, float2Int)
+
+
+squarel :: Num a => a -> a
+squarel x = x * x
+
+averagel :: Fractional a => a -> a -> a
+averagel x y = (x + y) / 2
+
+meanSquarel :: Fractional a => a -> a -> a
+meanSquarel x y = averagel (squarel x) (squarel y)
+
+absl :: (Ord t, Num t) => t -> t
+absl n
+    | n < 0     = absl (-n)
+    | otherwise = n
+
+improvel :: Fractional a => a -> a -> a
+improvel guess x = averagel guess (x / guess)
+
+goodEnough :: (Ord a, Fractional a) => a -> a -> Bool
+goodEnough guess x = absl (squarel guess - x) < 0.001
+
+tryl :: (Ord t, Fractional t) => t -> t -> t
+tryl guess x
+  | goodEnough guess x = guess
+  | otherwise = tryl (improvel guess x) x
+
+sqrtl :: Double -> Double
+sqrtl = tryl 1
+
+main :: IO ()
+main = print $ sqrtl 10
